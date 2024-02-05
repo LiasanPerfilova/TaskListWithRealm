2a717a61e6c081961f889f2cd44d78ca2299a4de
LiaisanPerfilova <perliaisan@gmail.com>
2024-02-05 18:12:46 -0500

    Initial Commit

---
+++ TaskListWithRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListWithRealm
+//
+//  Created by Liaisan on 2/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
